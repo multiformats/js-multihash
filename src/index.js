cf995b031976a4fa08846a5892d846a93c9b8129
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2016-12-07 14:53:40 +0100

    docs(api): first pass

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
+/**
+ * Multihash implementation in JavaScript.
+ *
+ * @module multihash
+ * @example
+ * const multihash = require('multihashes')
+ * const buf = new Buffer('0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33', 'hex')
+ *
+ * const encoded = multihash.encode(buf, 'sha1')
+ * console.log(encoded)
+ * // => <Buffer 11 14 0b ee c7 b5 ea 3f 0f db c9 5d 0d d4 7f 3c 5b c2 75 da 8a 33>
+ *
+ * const decoded = multihash.decode(encoded)
+ * console.log(decoded)
+ * // => {
+ * //      code: 17,
+ * //      name: 'sha1',
+ * //      length: 20,
+ * //      digest: <Buffer 0b ee c7 b5 ea 3f 0f db c9 5d 0d d4 7f 3c 5b c2 75 da 8a 33>
+ * //    }
+ *
+ */
 'use strict'
 
 const bs58 = require('bs58')
 
 const cs = require('./constants')
 
+/**
+ * Convert the given multihash to a hex encoded string.
+ *
+ * @param {Buffer} m
+ * @returns {string}
+ */
 exports.toHexString = function toHexString (m) {
   if (!Buffer.isBuffer(m)) {
     throw new Error('must be passed a buffer')
@@ -12,10 +40,22 @@ exports.toHexString = function toHexString (m) {
   return m.toString('hex')
 }
 
+/**
+ * Convert the given hex encoded string to a multihash.
+ *
+ * @param {string} s
+ * @returns {Buffer}
+ */
 exports.fromHexString = function fromHexString (s) {
   return new Buffer(s, 'hex')
 }
 
+/**
+ * Convert the given multihash to a base58 encoded string.
+ *
+ * @param {Buffer} m
+ * @returns {string}
+ */
 exports.toB58String = function toB58String (m) {
   if (!Buffer.isBuffer(m)) {
     throw new Error('must be passed a buffer')
@@ -24,6 +64,12 @@ exports.toB58String = function toB58String (m) {
   return bs58.encode(m)
 }
 
+/**
+ * Convert the given base58 encoded string to a multihash.
+ *
+ * @param {string} s
+ * @returns {Buffer}
+ */
 exports.fromB58String = function fromB58String (s) {
   let encoded = s
   if (Buffer.isBuffer(s)) {
@@ -33,7 +79,16 @@ exports.fromB58String = function fromB58String (s) {
   return new Buffer(bs58.decode(encoded))
 }
 
-// Decode a hash from the given Multihash.
+/**
+ * Decode a hash from the given multihash.
+ *
+ * @param {Buffer} buf
+ * @returns {Object} result
+ * @returns {number} result.code
+ * @returns {string} result.name
+ * @returns {number} result.length
+ * @returns {Buffer} result.digest
+ */
 exports.decode = function decode (buf) {
   exports.validate(buf)
 
@@ -47,8 +102,16 @@ exports.decode = function decode (buf) {
   }
 }
 
-// Encode a hash digest along with the specified function code.
-// Note: the length is derived from the length of the digest itself.
+/**
+ *  Encode a hash digest along with the specified function code.
+ *
+ * > **Note:** the length is derived from the length of the digest itself.
+ *
+ * @param {Buffer} digest
+ * @param {string|number} code
+ * @param {number} [length]
+ * @returns {Buffer}
+ */
 exports.encode = function encode (digest, code, length) {
   if (!digest || !code) {
     throw new Error('multihash encode requires at least two args: digest, code')
@@ -76,7 +139,12 @@ exports.encode = function encode (digest, code, length) {
   return Buffer.concat([new Buffer([hashfn, length]), digest])
 }
 
-// Converts a hashfn name into the matching code
+/**
+ * Converts a hash function name into the matching code.
+ * If passed a number it will return the number if it's a valid code.
+ * @param {string|number} name
+ * @returns {number}
+ */
 exports.coerceCode = function coerceCode (name) {
   let code = name
 
@@ -98,12 +166,22 @@ exports.coerceCode = function coerceCode (name) {
   return code
 }
 
-// Checks wether a code is part of the app range
+/**
+ * Checks wether a code is part of the app range
+ *
+ * @param {number} code
+ * @returns {boolean}
+ */
 exports.isAppCode = function appCode (code) {
   return code > 0 && code < 0x10
 }
 
-// Checks whether a multihash code is valid.
+/**
+ * Checks whether a multihash code is valid.
+ *
+ * @param {number} code
+ * @returns {boolean}
+ */
 exports.isValidCode = function validCode (code) {
   if (exports.isAppCode(code)) {
     return true
@@ -116,6 +194,13 @@ exports.isValidCode = function validCode (code) {
   return false
 }
 
+/**
+ * Check if the given buffer is a valid multihash. Throws an error if it is not valid.
+ *
+ * @param {Buffer} multihash
+ * @returns {undefined}
+ * @throws {Error}
+ */
 exports.validate = function validate (multihash) {
   if (!(Buffer.isBuffer(multihash))) {
     throw new Error('multihash must be a Buffer')
