922934cd77c57d9a83e32851fb019d4c2afa9453
Vasco Santos <vasco.santos@moxy.studio>
2021-08-24 15:42:19 +0200

    chore: update uint8arrays (#142)
    * chore: update uint8arrays
    
    * chore: update action
    
    * chore: remove typescript check action
    
    * chore: increase bundle size
    
    * chore: remove ts ignores
    
    * fix: cov is only supported in the main thread

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@
 const multibase = require('multibase')
 const varint = require('varint')
 const { names } = require('./constants')
-const uint8ArrayToString = require('uint8arrays/to-string')
-const uint8ArrayFromString = require('uint8arrays/from-string')
-const uint8ArrayConcat = require('uint8arrays/concat')
+const { toString: uint8ArrayToString } = require('uint8arrays/to-string')
+const { fromString: uint8ArrayFromString } = require('uint8arrays/from-string')
+const { concat: uint8ArrayConcat } = require('uint8arrays/concat')
 
 const codes = /** @type {import('./types').CodeNameMap} */({})
 
